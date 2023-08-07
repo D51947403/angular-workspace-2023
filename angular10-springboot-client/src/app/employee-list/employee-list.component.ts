ba5db2b25b0500931dd3f610e53a128a88409e93
D51947403 <singraul20codeblog@gmail.com>
2023-08-07 15:28:23 +0530

    employee list component updated

--- angular10-springboot-client/src/app/employee-list/employee-list.component.ts
+++ angular10-springboot-client/src/app/employee-list/employee-list.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { Employee } from '../employee';
+import { EmployeeService } from '../employee.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent {
+export class EmployeeListComponent implements OnInit {
 
+  employees: Observable<Employee[]> | undefined;
+
+  constructor (private employeeService :EmployeeService ,private router :Router){}
+  ngOnInit(): void {
+    this.reloadData();
+  }
+
+
+  reloadData(){
+    this.employees=this.employeeService.getEmployeeList();
+  }
+
+  deleteEmployee(id :number){
+    this.employeeService.deleteEmployee(id)
+        .subscribe(
+        (data :any) =>{
+           console.log(data);
+          this.reloadData();
+        },
+        (error :any) =>
+          console.log(error)
+        );
+  }
+
+  employeeDetails(id :number){
+    //console.log("employeeDetails "+id);
+   this.router.navigate(['details' ,id]);
+  }
+  
+  employeeUpdate(id :number){
+   // console.log("employeeDetails "+id);
+   this.router.navigate(['update' ,id]);
+  }
 }

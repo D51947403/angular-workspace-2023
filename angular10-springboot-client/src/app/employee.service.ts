f2caf3962ee0992a304267284b32cce94b77d0ff
D51947403 <singraul20codeblog@gmail.com>
2023-08-07 14:58:06 +0530

    employee service completed

--- angular10-springboot-client/src/app/employee.service.ts
+++ angular10-springboot-client/src/app/employee.service.ts
@@ -1,9 +1,28 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class EmployeeService {
+  private basUrl ="http://localhost:8080/ems-app/api/v1/employees";
+  constructor(private http:HttpClient) { }
+  
+  getEmployee(id:number):Observable<any> {
+    return this.http.get(this.basUrl+'/'+id);
+  }
+  createEmployee(employee :Object):Observable<Object> {
+    return this.http.post(this.basUrl ,employee);
+  }
+  updateEmployee(id:number ,value:any):Observable<Object>{
+   return this.http.put(this.basUrl+'/'+id,value);
+  }
+  deleteEmployee(id:number):Observable<any>{
+   return this.http.delete(this.basUrl+'/'+id ,{responseType : 'text'});
+   }
+  getEmployeeList():Observable<any>{
+    return this.http.get(this.basUrl);
+  }
 
-  constructor() { }
 }

f78c0e602a1e8d5b7b341ccdd511da58732cdc89
D51947403 <singraul20codeblog@gmail.com>
2023-08-07 15:25:57 +0530

    employee details component updated

--- angular10-springboot-client/src/app/employee-details/employee-details.component.ts
+++ angular10-springboot-client/src/app/employee-details/employee-details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { EmployeeService } from '../employee.service';
+import { Employee } from '../employee';
 
 @Component({
   selector: 'app-employee-details',
   templateUrl: './employee-details.component.html',
   styleUrls: ['./employee-details.component.css']
 })
-export class EmployeeDetailsComponent {
+export class EmployeeDetailsComponent implements OnInit{
+  id!: number;
+  employee!: Employee;
 
+  constructor (private route :ActivatedRoute, private router :Router, private employeeService :EmployeeService){}
+
+  ngOnInit(): void {
+    this.employee = new Employee();
+    this.id =this.route.snapshot.params['id'];
+
+    this.employeeService.getEmployee(this.id).subscribe(
+          (data :any) =>{
+           console.log(data);
+           this.employee=data;
+          },
+          (error :any) => {
+            console.log(error)
+          }
+    );
+  }
+
+  list(){
+    this.router.navigate(['employees']);
+  }
 }

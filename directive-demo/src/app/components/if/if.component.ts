875fa6070130d0efadff37f55d8d91542476acd8
D51947403 <singraul20codeblog@gmail.com>
2023-08-07 18:01:41 +0530

    Directive demo appication

---
+++ directive-demo/src/app/components/if/if.component.ts
@@ -0,0 +1,21 @@
+import { Component, NgZoneOptions, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-if',
+  templateUrl: './if.component.html',
+  styleUrls: ['./if.component.css']
+})
+export class IfComponent implements OnInit{
+  x:number; 
+  y:number;
+
+  constructor(){
+    this.x=10;
+    this.y=20;
+  }
+  
+  ngOnInit(): void {
+  
+  }
+
+}

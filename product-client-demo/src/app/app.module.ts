c427867d38027e55fe41df9c05d7dac1764099a5
D51947403 <singraul20codeblog@gmail.com>
2023-08-08 17:01:42 +0530

    useless component deleted

--- product-client-demo/src/app/app.module.ts
+++ product-client-demo/src/app/app.module.ts
@@ -3,15 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProductDataComponent } from './components/product-data/product-data.component';
 import { HttpClientModule } from '@angular/common/http';
 import { ProductDataService } from './services/product-data.service';
 import { FormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProductDataComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,

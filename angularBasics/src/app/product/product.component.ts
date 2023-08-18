f31b72e2f15da5da7e6cda371e2262d0c244b0e6
D51947403 <singraul20codeblog@gmail.com>
2023-08-18 18:32:27 +0530

    testing gitcopilot

--- angularBasics/src/app/product/product.component.ts
+++ angularBasics/src/app/product/product.component.ts
@@ -27,11 +27,18 @@ export class ProductComponent implements OnInit{
   }
 
   ngOnInit(): void {
-  
+   this.createProduct()
   }
 
 getProduct() {
   return this.products;
+}
+
+ createProduct() {
+   this.products.push({id:14,name:"Samsung S10"});
+   this.products.push({id:15,name:"Samsung S20"});
+   this.products.push({id:16,name:"Samsung S30"});
+   this.products.push({id:17,name:"Motorola S40"});
 }
 
 }
